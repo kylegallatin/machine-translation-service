ed6e6d48cc3b1721046caef83cccf54846c662ac
Kyle Gallatin <Kyle.Gallatin@pfizer.com>
2020-06-17 10:09:48 -0400

    removed os.path.join for url so it works on windows

--- download_models.py
+++ download_models.py
@@ -1,28 +1,34 @@
 import os
-import argparse 
+import argparse
 import urllib
 from urllib.request import urlretrieve
 from config import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--source', type=str, help='source language code')
-parser.add_argument('--target', type=str, help='sum the integers (default: find the max)')
+parser.add_argument("--source", type=str, help="source language code")
+parser.add_argument(
+    "--target", type=str, help="sum the integers (default: find the max)"
+)
 
-def download_language_model(source,target):
+
+def download_language_model(source, target):
     model = f"opus-mt-{source}-{target}"
     print(">>>Downloading data for %s to %s model..." % (source, target))
-    os.makedirs(os.path.join("data",model))
+    os.makedirs(os.path.join("data", model))
     for f in FILENAMES:
         try:
-            print(os.path.join(HUGGINGFACE_S3_BASE_URL,model,f))
-            urlretrieve(os.path.join(HUGGINGFACE_S3_BASE_URL,model,f),
-                                        os.path.join(MODEL_PATH,model,f))
+            print(os.path.join(HUGGINGFACE_S3_BASE_URL, model, f))
+            urlretrieve(
+                "/".join([HUGGINGFACE_S3_BASE_URL, model, f]),
+                os.path.join(MODEL_PATH, model, f),
+            )
             print("Download complete!")
         except urllib.error.HTTPError:
             print("Error retrieving model from url. Please confirm model exists.")
-            os.rmdir(os.path.join("data",model))
+            os.rmdir(os.path.join("data", model))
             break
-    
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     download_language_model(args.source, args.target)
