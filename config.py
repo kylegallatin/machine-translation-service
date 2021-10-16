bfba45d128eafbd3d3be0e3456de90545e8e088e
Ubuntu <ubuntu@ip-172-31-64-95.ec2.internal>
2021-10-16 08:38:11 +0000

    fix: use absolute path for model path

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 HUGGINGFACE_S3_BASE_URL="https://s3.amazonaws.com/models.huggingface.co/bert/Helsinki-NLP"
 FILENAMES = ["config.json","pytorch_model.bin","source.spm","target.spm","tokenizer_config.json","vocab.json"]
-MODEL_PATH = "data"
+MODEL_PATH = "/var/www/machine-translation-service/data"
+
