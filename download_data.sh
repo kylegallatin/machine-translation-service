f6bccacaccea1c0774f1b59fff16c373e337a403
Ubuntu <ubuntu@ip-172-31-64-95.ec2.internal>
2021-10-16 08:38:29 +0000

    chore: create script to download the models required

---
+++ download_data.sh
@@ -0,0 +1,40 @@
+mkdir data/
+
+python3 download_models.py --source en --target jap
+python3 download_models.py --source jap --target en
+
+python3 download_models.py --source en --target es
+python3 download_models.py --source es --target en
+
+python3 download_models.py --source en --target zh
+python3 download_models.py --source zh --target en
+
+python3 download_models.py --source en --target zh
+python3 download_models.py --source zh --target en
+
+python3 download_models.py --source it --target zh
+python3 download_models.py --source zh --target it
+
+python3 download_models.py --source en --target fr
+python3 download_models.py --source fr --target en
+
+python3 download_models.py --source en --target de
+python3 download_models.py --source de --target en
+
+python3 download_models.py --source es --target jap
+python3 download_models.py --source jap --target es
+
+python3 download_models.py --source es --target zh
+python3 download_models.py --source zh --target es
+
+python3 download_models.py --source es --target zh
+python3 download_models.py --source zh --target es
+
+python3 download_models.py --source it --target zh
+python3 download_models.py --source zh --target it
+
+python3 download_models.py --source es --target fr
+python3 download_models.py --source fr --target es
+
+python3 download_models.py --source es --target de
+python3 download_models.py --source de --target es
